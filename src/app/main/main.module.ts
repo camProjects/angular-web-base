520264ee0c6ced9c6defaa99adcdf81aabf97286
Cam Phan <cam.phan@enouvo.com>
2018-07-03 16:04:31 +0700

    main module

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
+import { StoreModule } from '@ngrx/store';
+import * as fromMain from './store/reducers';
 
 @NgModule({
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    StoreModule.forFeature('main', fromMain.reducers, { metaReducers: fromMain.metaReducers })
   ],
   declarations: []
 })
